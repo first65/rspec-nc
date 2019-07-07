4835b16230b989cee680f4bb5edebaf519d3983c
Otterpocket <fisix1@gmail.com>
2019-07-07 21:54:32 +0100

    brand rspec notifier and add sound

--- lib/nc.rb
+++ lib/nc.rb
@@ -2,19 +2,21 @@ require 'rspec/core/formatters/base_formatter'
 require 'terminal-notifier'
 
 class Nc < RSpec::Core::Formatters::BaseFormatter
-  SUCCESS_EMOJI = "\u2705"
-  FAILURE_EMOJI = "\u26D4"
+  SUCCESS_EMOJI = "\u2705".freeze
+  FAILURE_EMOJI = "\u26D4".freeze
 
   RSpec::Core::Formatters.register self, :dump_summary
 
   def dump_summary(notification)
     body = "Finished in #{notification.formatted_duration}\n#{notification.totals_line}"
-    title = if notification.failure_count > 0
-      "#{FAILURE_EMOJI} #{directory_name}: #{notification.failure_count} failed example#{notification.failure_count == 1 ? nil : 's'}"
+    if notification.failure_count > 0
+      title = "#{FAILURE_EMOJI} #{directory_name}: #{notification.failure_count} failed example#{notification.failure_count == 1 ? nil : 's'}"
+      sound = 'Frog'
     else
-      "#{SUCCESS_EMOJI} #{directory_name}: Success"
+      title = "#{SUCCESS_EMOJI} #{directory_name}: Success"
+      sound = 'Pop'
     end
-    notify title, body
+    notify title, body, sound
   end
 
   private
@@ -23,7 +25,11 @@ class Nc < RSpec::Core::Formatters::BaseFormatter
     File.basename File.expand_path '.'
   end
 
-  def notify(title, body)
-    TerminalNotifier.notify body, title: title
+  def notify(title, body, sound)
+    TerminalNotifier.notify body,
+      title: title,
+      sound: sound,
+      group: Process.pid,
+      appIcon: Rails.root / 'app' / 'assets' / 'images' / 'themes' / 'bruce' / 'favicon-96.png'
   end
 end
